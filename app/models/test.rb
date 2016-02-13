7eda562f4e114ad19318045acaa85b015d6334bd
David Bautista <gaijin.bushi.games@gmail.com>
2016-02-13 19:14:29 +0100

    Estilo de la página de overview de un examen.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,12 @@ class Test < ActiveRecord::Base
   belongs_to :user
   belongs_to :exam
   has_many :test_questions
+
+  def correct_answers_count
+    self.test_questions.where(correct: true).count
+  end
+
+  def incorrect_answers_count
+    self.test_questions.where(correct: false).count
+  end
 end

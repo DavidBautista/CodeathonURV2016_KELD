0e7ad4c728bc35df6fd9cde7163b2c2fb335316c
David Bautista <gaijin.bushi.games@gmail.com>
2016-02-14 09:31:36 +0100

    Añadida columna de posición al ranking.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,19 @@
 class DashboardController < ApplicationController
 
+  helper_method 'ranking_per_page'
+
   def index
 
   end
 
   def global_ranking
-    @users = User.paginate(:page => params[:page], :per_page => 2).order('experience DESC')
+    params[:page] ||=1
+    @users = User.paginate(:page => params[:page], :per_page => ranking_per_page).order('experience DESC')
+  end
+
+  protected
+  def ranking_per_page
+    20
   end
 
 end
